e6020ac368457110339389d5a11c6cb7a7b12d1c
kiethao <dangkiethao01@gmail.com>
2024-09-11 08:13:30 +0930

    Update Item Model

--- model/Items.js
+++ model/Items.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const menuItemSchema = new mongoose.Schema({
   name: String,
   description: String,
+  image: String,
   price: Number,
   category: String,
 });
